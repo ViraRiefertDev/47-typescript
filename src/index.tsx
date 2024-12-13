2fa32809b47d39f58ff5fe2f74e6d60151f6a277
ViraRiefertDev <vira.riefert.dev@gmail.com>
2024-12-13 06:52:02 +0100

    lesson08

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Lesson07 from './lessons/lesson07/Lesson07';
+import Lesson08 from './lessons/lesson08/Lesson08';
 
 
 const root = ReactDOM.createRoot(
@@ -8,7 +9,8 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <>
-    <Lesson07/>
+    <Lesson08/>
   </>
 );
+
 
